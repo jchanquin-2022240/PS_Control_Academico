8a8b3a1e19ed5d220819a28ce8077aa3d886d04c
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-13 15:36:14 -0600

    Creación de modelo de personas

---
+++ models/maestro.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const PersonaSchema = Schema({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre del maestro es obligatorio']
+    },
+    correo:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    cursos:{
+        type: String,
+        required: [true, 'El curso es obligatorio']
+    },
+    role:{
+        type: true,
+        enum: ["TEACHER_ROLE", "STUDENT_ROLE"],
+
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    }
+});
+
+
+module.exports = model('Persona', PersonaSchema);
