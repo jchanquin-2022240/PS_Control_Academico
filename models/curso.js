d8769d6cc8c05c9a9a8b70a3ce6aced86de30ae9
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-13 14:27:27 -0600

    se agregaron dependencias y creación de modelo cursos

---
+++ models/curso.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const CursoSchema = Schema({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre del curso es obligatorio']
+    },
+    codigoCurso:{
+        type: String,
+        required: [true, 'El codigo del curso es obligatorio']
+    },
+    descripcion:{
+        type: String,
+        required: [true, 'La descripción es obligatoria']
+    },
+    //validar si existe el maestro
+    maestro:{
+        type: String,
+        require: [true, 'El maestro es obligatorio']
+    },
+    estadoCurso:{
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = model('Curso', CursoSchema);
